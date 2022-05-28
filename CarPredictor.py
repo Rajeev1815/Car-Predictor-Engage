cc8ca40a115e20008ed545c840aca744aa756efa
Rajeev1815 <98849022+Rajeev1815@users.noreply.github.com>
2022-05-28 16:15:12 +0530

    Update CarPredictor.py

--- CarPredictor.py
+++ CarPredictor.py
@@ -1,5 +1,6 @@
 import streamlit as st  #importing libraries
 import pickle 
+import time
 model = pickle.load(open('RF_model.pkl','rb'))  #importing the model that was trained using Random forest algo
 
 def main(): #defining main function
